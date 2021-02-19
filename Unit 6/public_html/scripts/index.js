6558a228e88b899d743e093aa048fbb9332655fb
Graeme Woods <graemewoods202@gmail.com>
2021-02-18 20:20:13 -0500

    jquery

---
+++ Unit 6/public_html/scripts/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay: true,
+        prevArrow: '<div class="arrow-left"></div>',
+        nextArrow: '<div class="arrow-right"></div>'
+    });
+});
