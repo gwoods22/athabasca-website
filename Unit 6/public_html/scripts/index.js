7c398536fdbfee6d83e3844669a66d574d820a55
Graeme Woods <graemewoods202@gmail.com>
2021-03-18 21:12:49 -0400

    tweets

--- Unit 6/public_html/scripts/index.js
+++ Unit 6/public_html/scripts/index.js
@@ -4,4 +4,16 @@ $(document).ready(function(){
         prevArrow: '<div class="arrow-left"></div>',
         nextArrow: '<div class="arrow-right"></div>'
     });
+
+    var configProfile = {
+        "profile": {"screenName": 'GrowWithGraeme'},
+        "domId": 'tweets',
+        "maxTweets": 3,
+        "enableLinks": true, 
+        "showUser": true,
+        "showTime": true,
+        "showImages": true,
+        "lang": 'en'
+    };
+    twitterFetcher.fetch(configProfile);
 });
